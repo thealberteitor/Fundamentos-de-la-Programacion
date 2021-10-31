fad930790fbbd567d26d1f8ea9246189d52d0657
jorge-2 <92479920+jorge-2@users.noreply.github.com>
2021-10-31 19:21:51 +0100

    Max commun divisor

--- sesion2/Ejs Alberto/13.cpp
+++ sesion2/Ejs Alberto/13.cpp
@@ -2,13 +2,37 @@
 
 using namespace std;
 
+int mcd(int a, int b){
+	while (a != b){
+        	if (a > b){
+            		return mcd(a - b, b);
+        	}
+        	else{
+            		return mcd(a, b - a);
+        	}
+    	}
+    	return a;
+}
+
+
+int max_com_div(int a, int b) {
+	int aux;
+	while (b != 0) {
+		aux = b;
+		b = a % b;
+        	a = aux;
+    	}
+    	return a;
+}
+
+
 int main(){
 	
-	//Declaración de variables
+	//DeclaraciÃ³n de variables
 	int numero, unidades, decenas;
 	
 	//Entrada de datos
-	cout << "Introduce un número entero: " << endl;
+	cout << "Introduce un nÃºmero entero: " << endl;
 	cin >> numero;
 	
 	//Operaciones
@@ -18,7 +42,7 @@ int main(){
 	numero = numero /10;
 	
 	//Salida de datos
-	cout << "El número introducido es: " << numero << "  " << decenas << "  " << unidades << endl;
+	cout << "El nÃºmero introducido es: " << numero << "  " << decenas << "  " << unidades << endl;
 }
 	
 
